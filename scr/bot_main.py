1461aadc795f4a2a0114d8f9ccaa2cc40ac52900
Mohammd Alavi <yourhonor1996@gmail.com>
2021-08-11 01:04:25 +0430

    first bot

---
+++ scr/bot_main.py
@@ -0,0 +1,26 @@
+token = '1891854677:AAEDNLc4q9cbtfHMI0JoBKviVx6gFLsApb4'
+
+from telegram import (Update, ReplyKeyboardMarkup)
+from telegram.ext import Updater, dispatcher
+from telegram.ext import (
+    CommandHandler, MessageHandler, Filters,
+    ConversationHandler, CallbackContext)
+import logging
+
+
+updater = Updater(token= token, use_context= True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+                    level= logging.INFO)
+
+
+def start(update:Update, context:CallbackContext):
+    # context.bot.send_message(chat_id= update.effective_chat.id, text= "I'm a botm please talk to me !")
+    update.message.reply_text("I'm a bot. hey!")
+    update.message.reply_document()
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
+updater.idle()
