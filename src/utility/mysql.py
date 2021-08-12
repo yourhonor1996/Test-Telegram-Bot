c8a31ab260e59456352cf29755360d602e72fe19
Mohammd Alavi <yourhonor1996@gmail.com>
2021-08-12 19:22:59 +0430

    did these: - rename sqlconnections - minor changes to functions

--- src/utility/mysql.py
+++ src/utility/mysql.py
@@ -5,13 +5,6 @@ from src.utility.util import logger
 
 
 
-commands = []
-database_query_filename = 'queries.sql'
-dirname = settings.QUERIES_DIR / database_query_filename
-
-with open(str(dirname), 'r') as file:
-    commands.append(file.read())
-
 
 def load_commands(query_name:str):
     '''loads the commands from a certain query filename in the queries folder'''
@@ -21,7 +14,7 @@ def load_commands(query_name:str):
         commands = file.read()
     return commands
 
-def run_sql_command(commands: "str | list[str]"):
+def run_sql(commands: "str | list[str]"):
     if type(commands) is str:
         commands = [commands]
     
@@ -29,24 +22,19 @@ def run_sql_command(commands: "str | list[str]"):
         connection = sql.connect(**settings.MYSQL_CONNECTION)
         if connection.is_connected():
             db_Info = connection.get_server_info()
-            # print("Connected to MySQL Server version ", db_Info, '\n')
             logger('Connected to MySQL Server version')
             cursor = connection.cursor()
             records = []
             for command in commands:
                 for result in cursor.execute(command, multi=True):
                     if result.with_rows:
-                        # print(f"Statement executed: {result.statement}")
                         logger(f"Statement executed: {result.statement}")
-                        # print(result.fetchall())
                         records += result.fetchall()
-                    # else:
-                        # print("Number of rows affected by statement '{}': {}".format(
             # return records
-            print(records)
+            if records:
+                print(records)
     except Error as e:
         logger(f"Error while connecting to MySQLL {e}\n")
-
     finally:
         if connection.is_connected():
             cursor.close()
@@ -57,8 +45,10 @@ def run_sql_command(commands: "str | list[str]"):
         
 
 def main():
-    run_sql_command(commands)
+    run_sql(load_commands('queries'))
     
     
 if __name__ == '__main__':
     main()
+    
+    
